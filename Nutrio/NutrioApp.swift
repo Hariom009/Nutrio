222f392c7a3279dcb7c024fde56f4f983447370d
Hariom009 <hariomconnects03@gmail.com>
2025-06-13 12:44:04 +0530

    Initial Commit

---
+++ Nutrio/NutrioApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutrioApp.swift
+//  Nutrio
+//
+//  Created by Hari's Mac on 13.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct NutrioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NutrioTests/NutrioTests.swift
@@ -0,0 +1,17 @@
+//
+//  NutrioTests.swift
+//  NutrioTests
+//
+//  Created by Hari's Mac on 13.06.2025.
+//
+
+import Testing
+@testable import Nutrio
+
+struct NutrioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
